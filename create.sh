0a26c66be4d171ce1a5a3eed9ea62ffe2abe6f4a
ghostry <ghostry@ghostry.cn>
2019-04-25 15:05:04 +0800

    del file

--- create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker run -v /data/workbench:/data -d --name workbench ghostry/workbench
-docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' workbench
-./use.sh
--- use.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker start workbench
-docker exec -it workbench zsh
